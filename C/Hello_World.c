048336daee36abce02040c7e0ed2a8985658e952
Wellington P. Silva <wellingtonps@gmail.com>
2021-08-28 11:18:56 -0300

    GOLANG/README_GOLANG.txt

---
+++ C/Hello_World.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int numero=0;
+int main(){
+    printf("Hello World\n");
+    system("PAUSE");
+}
---
+++ C/Teste_visual_studio_code.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int numero=0;
+int main(){
+    printf("Digite um numero\n");
+    scanf("%d",&numero);
+    printf("O numero informado foi: %d\n",numero);
+    system("PAUSE");
+}
