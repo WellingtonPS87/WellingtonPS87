c2af25f7aa8ced0ff95d59cdbaa49a040feb34e3
Wellington P. Silva <wellingtonps@gmail.com>
2021-08-28 11:46:45 -0300

    README.md

--- C/Hello_World2.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int numero=0;
-int main(){
-    printf("Hello World_2\n");
-    system("PAUSE");
-}
--- C/Teste_visual_studio_code.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int numero=0;
-int main(){
-    printf("Digite um numero\n");
-    scanf("%d",&numero);
-    printf("O numero informado foi: %d\n",numero);
-    system("PAUSE");
-}
